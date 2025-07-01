77186289c50542a0ee0e93b2a4396281ec749684
maisan <Maisan.work@hotmail.com>
2025-07-01 02:00:51 -0700

    update user_navigation_view

--- lib/features/user_navigation/view/user_navigation_view.dart
+++ lib/features/user_navigation/view/user_navigation_view.dart
@@ -1,143 +1,96 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:easy_localization/easy_localization.dart';
+
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:wellnesstrackerapp/global/router/app_router.gr.dart';
+import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:wellnesstrackerapp/global/theme/theme_x.dart';
-import 'package:wellnesstrackerapp/global/utils/constants.dart';
 import 'package:wellnesstrackerapp/global/widgets/main_app_bar.dart';
+import '../../adds_and_offers/view/adds_and_offers_view.dart';
+import '../../dashboard/view/dashboard_view.dart';
+import '../../profile/view/profile_view.dart';
 
-abstract class UserNavigationViewCallbacks {
-  void onBottomTab(
-    int previousIndex,
-    int currentIndex,
-    TabsRouter tabsRouter,
-  );
-}
-
-@RoutePage()
-class UserNavigationView extends StatelessWidget {
+class UserNavigationView extends StatefulWidget {
   const UserNavigationView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const UserNavigationPage();
-  }
+  State<UserNavigationView> createState() => _UserNavigationViewState();
 }
 
-class UserNavigationPage extends StatefulWidget {
-  const UserNavigationPage({super.key});
+class _UserNavigationViewState extends State<UserNavigationView> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  final List<Widget> pages = const [
+    ProfileView(),
+    DashboardView(),
+    AddsAndOffersView(),
+  ];
+
+  bool get isRtl => context.locale.languageCode == 'ar';
 
   @override
-  State<UserNavigationPage> createState() => _UserNavigationPageState();
-}
-
-class _UserNavigationPageState extends State<UserNavigationPage>
-    implements UserNavigationViewCallbacks {
-  int currentIndex = 1;
-
-  @override
-  void onBottomTab(int previousIndex, int currentIndex, TabsRouter tabsRouter) {
-    tabsRouter.setActiveIndex(currentIndex);
-    setState(() {
-      this.currentIndex = currentIndex;
-    });
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: pages.length, vsync: this, initialIndex: 1);
   }
 
-  Widget getBottomBarIcon(IconData icon, {required bool isSelected}) {
-    final color = isSelected ? context.cs.primary : context.cs.secondary;
-    return Icon(icon, color: color);
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AutoTabsScaffold(
-      appBarBuilder: (context, tabsRouter) {
-        return MainAppBar();
-      },
-      routes: const [ProfileRoute(), DashboardRoute(), AddsAndOffersRoute()],
-      extendBody: true,
-      resizeToAvoidBottomInset: true,
-      bottomNavigationBuilder: (context, tabsRouter) {
-        return DecoratedBox(
-          decoration: BoxDecoration(
-            color: context.cs.surface,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-            boxShadow: kElevationToShadow[4],
+    final icons = [Icons.person, Icons.home, Icons.local_offer];
+    final labels = ['profile'.tr(), 'home'.tr(), 'adds_and_offers'.tr()];
+
+    final rtlIcons = icons.reversed.toList();
+    final rtlLabels = labels.reversed.toList();
+
+    return Scaffold(
+      appBar: MainAppBar(),
+      body: TabBarView(
+        controller: _tabController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: isRtl ? pages.reversed.toList() : pages,
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: context.cs.surface,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 12,
+              offset: Offset(0, -2),
+            )
+          ],
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(16),
+            topRight: Radius.circular(16),
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-            child: Padding(
-              padding: AppConstants.padding20,
-              child: GNav(
-                  selectedIndex: currentIndex,
-                  onTabChange: (value) {
-                    onBottomTab(tabsRouter.activeIndex, value, tabsRouter);
-                  },
-                  tabBorderRadius: 15,
-                  curve: Curves.easeOutExpo,
-                  duration: Duration(milliseconds: 500),
-                  gap: 8,
-                  activeColor: context.cs.primary,
-                  iconSize: 24,
-                  tabBackgroundColor: context.cs.primary.withValues(alpha: 0.3),
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  tabs: [
-                    GButton(
-                      icon: Icons.person,
-                      text: 'profile'.tr(),
-                    ),
-                    GButton(
-                      icon: Icons.home,
-                      text: 'home'.tr(),
-                    ),
-                    GButton(
-                      icon: Icons.local_offer,
-                      text: 'adds_and_offers'.tr(),
-                    )
-                  ]),
-            ),
+        ),
+        child: MotionTabBar(
+          labels: isRtl ? rtlLabels : labels,
+          icons: isRtl ? rtlIcons : icons,
+          initialSelectedTab: isRtl ? rtlLabels[1] : labels[1],
+          tabSize: 60,
+          tabBarHeight: 65,
+          textStyle: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w600,
+            color: context.cs.primary,
           ),
-        );
-        // return DecoratedBox(
-        //   decoration: BoxDecoration(
-        //     borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-        //     boxShadow: kElevationToShadow[2],
-        //   ),
-        //   child: ClipRRect(
-        //     borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-        //     child: BottomNavigationBar(
-        //       currentIndex: tabsRouter.activeIndex,
-        //       onTap: (index) {
-        //         onBottomTab(tabsRouter.activeIndex, index, tabsRouter);
-        //       },
-        //       items: [
-        //         BottomNavigationBarItem(
-        //           icon: getBottomBarIcon(
-        //             Icons.person,
-        //             isSelected: tabsRouter.activeIndex == 0,
-        //           ),
-        //           label: 'profile'.tr(),
-        //         ),
-        //         BottomNavigationBarItem(
-        //           icon: getBottomBarIcon(
-        //             Icons.home,
-        //             isSelected: tabsRouter.activeIndex == 1,
-        //           ),
-        //           label: 'home'.tr(),
-        //         ),
-        //         BottomNavigationBarItem(
-        //           icon: getBottomBarIcon(
-        //             Icons.local_offer,
-        //             isSelected: tabsRouter.activeIndex == 2,
-        //           ),
-        //           label: 'adds_and_offers'.tr(),
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // );
-      },
+          tabIconColor: Colors.grey.shade500,
+          tabIconSelectedColor: context.cs.primary,
+          tabSelectedColor: context.cs.primary.withOpacity(0.15),
+          tabBarColor: context.cs.surface,
+          useSafeArea: true,
+          onTabItemSelected: (index) {
+            setState(() {
+              _tabController.index = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
