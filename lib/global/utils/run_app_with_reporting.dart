d080352a55aade438fac49e8d54badffb87b4c58
maisan <Maisan.work@hotmail.com>
2025-07-07 03:47:00 -0700

    notification configure

--- lib/global/utils/run_app_with_reporting.dart
+++ lib/global/utils/run_app_with_reporting.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
-
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wellnesstrackerapp/features/app/app.dart';
 import 'package:wellnesstrackerapp/global/blocs/app_bloc_observer.dart';
 import 'package:wellnesstrackerapp/global/di/di.dart';
+import '../../firebase_options.dart';
 
 Future<void> runAppWithReporting() async {
   await runZonedGuarded(
@@ -14,6 +15,10 @@ Future<void> runAppWithReporting() async {
       WidgetsFlutterBinding.ensureInitialized();
       EasyLocalization.ensureInitialized();
       await configureDependencies();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
 
       final app = App();
 
